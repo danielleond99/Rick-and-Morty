07235be43fb0d44503f48fa431ff52c7363472d2
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2024-06-14 13:18:02 -0400

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:rick_and_morty/screens/export.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -10,11 +12,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Rick and Morty',
+      home: SplashScreen(),
     );
   }
 }
